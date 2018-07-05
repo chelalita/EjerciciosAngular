86ea567fd85f6a117909819ca78559db0f410098
Nestor Kauil <neto1xix@gmail.com>
2018-07-05 17:19:55 -0500

    - ADD Pipes para manejo de numeros, monedas, porcentajes, json, promesas - ADD Pipe para las fechas normales y personalizadas - ADD import para configurar el idioma en todo el proyecto

--- 03-pipes/src/app/app.component.ts
+++ 03-pipes/src/app/app.component.ts
@@ -7,14 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  nombre  = "Nestor";
+  nombre  = 'Nestor';
 
-  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   PI = Math.PI;
 
   a:number = 0.234;
 
+  salario = 1234.5;
 
+  heroe = {
+      nombre: 'Logan',
+      clave: 'Wolverine',
+      edad: 500,
+      direccion: {
+          calle: 'Primera',
+          casa: '19'
+      }
+  };
+
+  valorDePromesa = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Llego la data!'), 3500);
+  });
+
+  fecha = new Date();
 
 }
--- 03-pipes/src/app/app.module.ts
+++ 03-pipes/src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core'; // import LOCALE_ID
 
+// import para configurar el idioma de la fecha en español
 import { AppComponent } from './app.component';
+import localEs from '@angular/common/locales/es';
+import { registerLocaleData} from '@angular/common';
+registerLocaleData(localEs);
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{
+        provide: LOCALE_ID, useValue: 'es' // Se agrega la variable LOCALE_ID y se configura el idioma
+      }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
