a03aae376d09a42c4ea48203d158a0c41fa9c894
Nestor Kauil <neto1xix@gmail.com>
2018-07-05 02:11:51 -0500

    - ADD Difentes tipos de pipes y su uso

---
+++ 03-pipes/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  nombre  = "Nestor";
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  PI = Math.PI;
+
+  a:number = 0.234;
+
+
+
+}
